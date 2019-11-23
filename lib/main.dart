388fea6ebfab200283db866ff5b5c6acc2f923a9
pocari <caffelattenonsugar@gmail.com>
2019-11-23 15:00:52 +0900

    Use an AnimatedWidget

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,15 @@ Route _createRoute() {
   return PageRouteBuilder(
     pageBuilder: (context, animation, secondaryAnimation) => Page2(),
     transitionsBuilder: (context, animation, secondaryAnimation, child) {
-      return child;
+      var begin = Offset(0.0, 1.0);
+      var end = Offset.zero;
+      var tween = Tween(begin: begin, end: end);
+      var offsetAnimation = animation.drive(tween);
+
+      return SlideTransition(
+        position: offsetAnimation,
+        child: child,
+      );
     },
   );
 }
