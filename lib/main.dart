afdbfdd16e6cd8a8d0e4d14c8cfbe6217e7f1a9c
pocari <caffelattenonsugar@gmail.com>
2019-11-23 14:49:08 +0900

    Set up a PageRouteBuilder

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,41 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(home: Page1()));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class Page1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return null;
+    return Scaffold(
+      appBar: AppBar(title: Text("PageRouteAnimation")),
+      body: Center(
+        child: RaisedButton(
+          child: Text("Go!"),
+          onPressed: () {
+            Navigator.of(context).push(_createRoute());
+          },
+        ),
+      ),
+    );
+  }
+}
+
+Route _createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => Page2(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return child;
+    },
+  );
+}
+
+class Page2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("sample")),
+      body: Center(
+        child: Text("Page 2"),
+      ),
+    );
   }
 }
